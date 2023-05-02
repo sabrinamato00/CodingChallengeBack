9cba2423f20b5da063a55039b4dac3610644cf4a
sabirnamato00 <sabrinamato00@gmail.com>
2023-05-02 20:58:45 -0300

    s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 
